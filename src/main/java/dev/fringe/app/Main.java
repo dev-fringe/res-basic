e8ef2e47dd836c0c0fd81b7553abcea49cdd40dc
Lee Hak Do <dev.fringe@gmail.com>
2021-04-09 13:50:45 +0900

    ss

--- src/main/java/dev/fringe/app/Main.java
+++ src/main/java/dev/fringe/app/Main.java
@@ -9,24 +9,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.BufferingClientHttpRequestFactory;
+import org.springframework.http.client.InterceptingClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 import dev.fringe.app.supprt.ApiRestLoggingRequestInterceptor;
 
 public class Main implements InitializingBean {
-	
+
 	@Value("${url:https://api.upbit.com/v1/market/all?isDetails=false}") String url;
 	@Autowired RestTemplate restTemplate;
-	
+
 	@Bean
 	public RestTemplate restTemplate() {
 		RestTemplate restTemplate = new RestTemplate();
-        restTemplate.setInterceptors(Collections.singletonList(new ApiRestLoggingRequestInterceptor()));
+		restTemplate.setRequestFactory(new InterceptingClientHttpRequestFactory( new BufferingClientHttpRequestFactory(new SimpleClientHttpRequestFactory()), Collections.singletonList(new ApiRestLoggingRequestInterceptor())));
 		return restTemplate;
 	}
+
 	public static void main(String[] args) {
 		new AnnotationConfigApplicationContext(Main.class);
 	}
+
 	public void afterPropertiesSet() throws Exception {
 		List<Map<String, Object>> list = restTemplate.getForObject(url, List.class);
 		for (Map<String, Object> market : list) {
--- src/main/java/dev/fringe/app/supprt/ApiRestLoggingRequestInterceptor.java
+++ src/main/java/dev/fringe/app/supprt/ApiRestLoggingRequestInterceptor.java
@@ -2,8 +2,6 @@ package dev.fringe.app.supprt;
 
 import java.io.IOException;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpRequest;
 import org.springframework.http.client.ClientHttpRequestExecution;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
@@ -15,10 +13,7 @@ import org.springframework.util.FileCopyUtils;
  */
 public class ApiRestLoggingRequestInterceptor implements ClientHttpRequestInterceptor {
 
-	private static final Logger requestLog = LogManager.getLogger("request");
-	private static final Logger responseLog = LogManager.getLogger("response");
 
-	@Override
 	public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution) throws IOException {
 		ClientHttpResponse response = execution.execute(request, body);
 		log(request, body, response);
@@ -26,13 +21,7 @@ public class ApiRestLoggingRequestInterceptor implements ClientHttpRequestInterc
 	}
 
 	private void log(HttpRequest request, byte[] body, ClientHttpResponse response) throws IOException {
-		if (requestLog.isInfoEnabled()) {
 			System.out.println(new String(body));
-			requestLog.info(request.getURI().toString() + " - " + new String(body));
-		}
-		if (responseLog.isInfoEnabled()) {
 			System.out.println( new String(FileCopyUtils.copyToByteArray(response.getBody())));
-			responseLog.info(request.getURI().toString() + " - " + new String(FileCopyUtils.copyToByteArray(response.getBody())));
-		}
 	}
 }
